b6081c5815cc47010776cc612e52753cf900739c
Li Arolf Rey <liarolfrrey@gmail.com>
2017-08-18 21:53:25 +0800

    Add global style

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -7,4 +7,15 @@ import App from './components/App';
 
 mobx.useStrict(true);
 
+injectGlobal`
+  @import url('https://fonts.googleapis.com/css?family=Raleway');
+
+  body {
+    margin: 0px;
+    padding: 0px;
+    height: 100vh;
+    width: 100vw;
+  }
+`
+
 ReactDOM.render(<App />, document.getElementById('mount-point'));
