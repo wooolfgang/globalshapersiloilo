8ddf0ca683f14ce58a06ae0306b6566d337bf0bb
Li Arolf Rey <liarolfrrey@gmail.com>
2017-08-21 09:08:32 +0800

    Remove divider component

--- src/frontend/components/App.jsx
+++ src/frontend/components/App.jsx
@@ -22,7 +22,6 @@ class App extends React.Component {
         <Header />
         <HeroContainer />
         <MainContainer />
-        <Divider />
         <LocalListingsContainer />
         <Footer />
       </StyledApp>
