59d5d87a3fa5dd73c23bec00768e9e5f5e6e193c
Li Arolf Rey <liarolfrrey@gmail.com>
2017-08-21 10:37:38 +0800

    Add playfair display font

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -8,7 +8,7 @@ import App from './components/App';
 mobx.useStrict(true);
 
 injectGlobal`
-  @import url('https://fonts.googleapis.com/css?family=Roboto|Raleway');
+  @import url('https://fonts.googleapis.com/css?family=Roboto|Raleway|Playfair+Display');
 
   body {
     margin: 0px;
