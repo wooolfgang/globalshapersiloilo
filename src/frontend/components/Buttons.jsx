0ce06e49aefb0206bf820653274934ea07ee5ac3
Li Arolf Rey <liarolfrrey@gmail.com>
2017-08-21 10:37:23 +0800

    Add Secondary button

--- src/frontend/components/Buttons.jsx
+++ src/frontend/components/Buttons.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledPrimary = styled.button`
-  color: #375D81;
+  color: #00529B;
   background: white;
   border: 2px solid white;
   border-radius: 5px;
@@ -10,6 +10,7 @@ const StyledPrimary = styled.button`
   padding: 8px;
   cursor: pointer;
   transition: .3s;
+  font-family: 'Roboto', 'sans-serif';
 
   :focus {
     outline: none;
@@ -21,8 +22,27 @@ const StyledPrimary = styled.button`
   }
 `
 
+const StyledSecondary = styled.button`
+  color: #00529B;
+  background: none;
+  border: 2px solid #00529B;
+  font-size: 22px;
+  padding: 0px 8px;
+  cursor: pointer;
+  transition: .3s;
+  font-family: 'Roboto', 'sans-serif';
+
+  :focus {
+    outline: none;
+  }
+`
+
 const Primary = ({ children }) => (
   <StyledPrimary> {children} </StyledPrimary>
 );
 
-export { Primary };
+const Secondary = ({ children }) => (
+  <StyledSecondary> {children} </StyledSecondary>
+);
+
+export { Primary, Secondary };
