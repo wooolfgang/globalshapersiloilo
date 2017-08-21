bc2dc1f2252583b72e01317c8038172fbe90f71e
Li Arolf Rey <liarolfrrey@gmail.com>
2017-08-21 08:22:20 +0800

    Update global styles

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -8,13 +8,16 @@ import App from './components/App';
 mobx.useStrict(true);
 
 injectGlobal`
-  @import url('https://fonts.googleapis.com/css?family=Raleway');
+  @import url('https://fonts.googleapis.com/css?family=Roboto|Raleway');
 
   body {
     margin: 0px;
     padding: 0px;
     height: 100vh;
     width: 100vw;
+    font-family: 'Roboto', 'sans-serif';
+    color: #333;
+    overflow-x: hidden;
   }
 `
 
