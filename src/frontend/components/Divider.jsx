a8490f8e4b37ee3e5096d0d94fef5f43f1695bac
Li Arolf Rey <liarolfrrey@gmail.com>
2017-08-21 08:21:55 +0800

    Add Divider component

---
+++ src/frontend/components/Divider.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  border-top: 1px solid #183152;
+  width: 60%;
+  margin: auto;
+`
+
+const Divider = () => (
+  <StyledDiv/>
+)
+
+export default Divider;
