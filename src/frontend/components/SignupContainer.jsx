c06596ead2ac4307b8e1cccdeb3d6637982a99b8
Li Arolf Rey <liarolfrrey@gmail.com>
2017-08-22 23:28:20 +0800

    Update SignupContainer styles

--- src/frontend/components/SignupContainer.jsx
+++ src/frontend/components/SignupContainer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { observer, inject } from 'mobx-react';
 import styled from 'styled-components';
 import { Close } from './Buttons';
+import loginWithFacebook from '../assets/loginfb.png';
 
 const StyledDiv = styled.div`
   position: fixed;
@@ -16,52 +17,74 @@ const StyledDiv = styled.div`
   z-index: 9999;
   overflow: hidden;
 
-  form {
-    padding: 10px;
-    color: #375D81;
-    font-size: 20px;
-    text-align: center;
-    transform: translate(0, 0);
+  ${props => !props.viewed && 'display: none;'}
+`;
+
+const LeftFloat = styled.div`
+  background: #375D81;
+  height: 96%;
+  border: 2px solid #375D81;
+  flex: 2;
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: white;
+    
+  h2 {
+    font-family: 'Playfair Display', 'serif';
   }
+`
+
+const Form = styled.form`
+  padding: 10px;
+  font-size: 20px;
+  text-align: center;
+  transform: translate(0, 0);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+
+  input[type="submit"] {
+    background: DarkOrange;
+    padding: 5px 8px;
+    font-size: 17px;
+    border: none;
+    cursor: pointer;
+    width: 100px;
+    color: white;
+    font-family: 'Roboto', 'sans-serif';
+  }
+`
+
+const FormRow = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 80%;
 
   input {
     height: 25px;
     font-size: 15px;
     margin-bottom: 30px;
-    border: 1px solid #375D81;
     text-align: center;
-    border-radius: 5px;
+    font-family: 'Raleway', 'sans-serif';
+    flex: 2;
   }
 
-  h2 {
-    text-align: center;
+  span {
+    font-size: 15px;
+    font-family: 'Raleway', 'sans-serif';
+    flex: 1;
   }
-
-  input[type="submit"] {
-    background-color: #375D81;
-    width: 100px;
-    color: white;
-  }
-
-  ${props => !props.viewed && 'display: none;'}
-`;
-
-const Modal = styled.div`
-  width: 650px;
-  height: 500px;
-  background-color: #E1E6FA;
-  margin-top: -30vh;
-  display: flex;
-  border-radius: 10px;
 `
 
-
-const LeftFloat = styled.div`
-  height: 96%;
-  background-color: #C4D7ED;
-  flex: 2;
-  border-radius: 10px;
-  margin: 10px;
+const Modal = styled.div`
+  width: 700px;
+  height: 500px;
+  background-color: white;
+  margin-top: -30vh;
+  display: flex;
 `
 
 const MiddleFloat = styled.div`
@@ -71,20 +94,19 @@ const MiddleFloat = styled.div`
 
 const RightFloat = styled.div`
   height: 96%;
-  background-color: #C4D7ED;
+  border: 2px solid #375D81;  
   flex: 1;
-  border-radius: 10px;
   margin: 10px;
+  padding: 0px 7px;
 
   img {
-    height: 26px;
-    width: 175px;
+    width: 200px;
+    height: auto;
   }
 
   a {
     position: fixed;
     top: 47%;
-    padding: 10px;
   }
 `
 
@@ -92,33 +114,33 @@ const SignupContainer = ({ viewed, rootStore }) => (
   <StyledDiv viewed={rootStore.uiStore.signupModalViewed} >
     <Modal>
       <LeftFloat>
-        <h2>Signup here</h2>
-        <form action="POST">
-          Name:
-          <div>
-            <input type="text" name="name" placeholder="*Name" required />
-          </div>
-          Email:
-          <div>
-            <input type="email" name="email" placeholder="*Email" required />
-          </div>
-          Birthdate:
-          <div>
-            <input type="date" name="birthdate" placeholder="*Birthdate" />
-          </div>
-          Occupation:
-          <div>
-            <input type="text" name="occupation" placeholder="*Occupation" />
-          </div>
+        <h2>Volunteer your skills.</h2>
+        <Form action="POST">
+          <FormRow>
+            <span> Full Name: </span>
+            <input type="text" name="name" placeholder="Mark John" required />
+          </FormRow>
+          <FormRow>
+            <span> Email: </span>
+            <input type="email" name="email" placeholder="john@email.com" required />
+          </FormRow>
+          <FormRow>
+            <span> Birthday: </span>
+            <input type="date" name="birthdate" placeholder="" />
+          </FormRow>
+          <FormRow>
+            <span> Skills: </span>
+            <input type="text" name="occupation" placeholder="Software Engineer" />
+          </FormRow>
           <input type="submit" value="Submit" />
-        </form>
+        </Form>
       </LeftFloat>
       <MiddleFloat>
         <h3>or</h3>
       </MiddleFloat>
       <RightFloat>
         <Close onClick={rootStore.uiStore.onSignupModalView}>x</Close>
-        <a href="#"><img src="../src/frontend/assets/loginfb.png" alt="..." /> </a>
+        <a href="#"><img src={loginWithFacebook} alt="..." /> </a>
       </RightFloat>
     </Modal>
   </StyledDiv>
