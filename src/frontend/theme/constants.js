09db98b2fed8977d8e3222141b56a65604651368
Li Arolf Rey <liarolfrrey@gmail.com>
2017-10-04 11:47:25 +0800

    Add more brand colors on theme

--- src/frontend/theme/constants.js
+++ src/frontend/theme/constants.js
@@ -2,7 +2,9 @@ const colors = {
   primary: '#00244d',
   secondary: '#005C9C',
   tertiary: '#09133b',
-  footer: '#0a133c',
+  footer: '#FAFAFA',
+  buttonPrimary: '#4E7AC7',
+  buttonPrimaryDarker: '#3a68b9',
 };
 
 export default colors;
