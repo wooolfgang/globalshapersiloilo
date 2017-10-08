34903c0e3018cb0ca859b3f7a457d8d4a83fa8c9
Clyne Paulo Fuentes <general_clyne@yahoo.com>
2017-10-09 02:43:45 +0800

    Add oauth2, facebook and google strategy on auth service

--- src/backend/services/authentication.js
+++ src/backend/services/authentication.js
@@ -1,6 +1,9 @@
 import auth from 'feathers-authentication';
 import jwt from 'feathers-authentication-jwt';
 import local from 'feathers-authentication-local';
+import oauth2 from 'feathers-authentication-oauth2';
+import { Strategy as FBStrategy } from 'passport-facebook';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20'
 
 function authentication() {
   return function execute() {
@@ -8,12 +11,33 @@ function authentication() {
 
     app.configure(auth(app.get('auth')));
     app.configure(local());
+    app.configure(oauth2({
+      name: 'facebook',
+      Strategy: FBStrategy,
+      service: 'api/users',
+      clientID: '338814276566702',
+      clientSecret: 'e286376f8175bcb8fc549224fc73f3cc',
+      callbackURL: 'http://localhost:3000/auth/facebook/callback',
+      successRedirect: 'http://localhost:3000/#/signup',
+      scope: ['public_profile', 'email'],
+      profileFields: ['email', 'gender', 'displayName'],
+    }));
+    app.configure(oauth2({
+      name: 'google',
+      Strategy: GoogleStrategy,
+      service: 'api/users',
+      clientID: '206616668311-3o0ls5sopak3e02vhkmvhpjnl9gqlk5f.apps.googleusercontent.com',
+      clientSecret: 'pW3jXSpRYbpp0JgdEmWuDapR',
+      callbackURL: 'http://localhost:3000/auth/google/callback',
+      successRedirect: 'http://localhost:3000/#/signup',
+      scope: ['profile', 'email'],
+      profileFields: ['email', 'gender', 'displayName'],
+    }));
     app.configure(jwt());
-
     app.service('api/authentication').hooks({
       before: {
         create: [
-          auth.hooks.authenticate(['jwt', 'local']),
+          auth.hooks.authenticate(['jwt', 'local', 'facebook', 'google']),
         ],
         remove: [
           auth.hooks.authenticate('jwt'),
