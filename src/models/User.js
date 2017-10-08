f9dab42b1334f251f90a13c9a69430f9d41f21eb
Clyne Paulo Fuentes <general_clyne@yahoo.com>
2017-10-09 02:39:03 +0800

    Temporarily Change User Schema

--- src/models/User.js
+++ src/models/User.js
@@ -8,11 +8,13 @@ class User extends Model {
       password: Joi.string().min(6).max(30).required(),
       email: Joi.string().email().required(),
       fullName: Joi.string().required(),
-      birthDay: Joi.string().required(),
+      birthDay: Joi.string(),
+      // birthDay: Joi.string().required(), temporarily
       address: Joi.string(),
       phoneNumber: Joi.string(),
       occupation: Joi.string(),
       affiliation: Joi.string(),
+      provider: Joi.string(),
     };
   }
 }
