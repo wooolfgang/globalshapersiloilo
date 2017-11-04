b9940fc3676f8ee5ed425aebd7ada20eb5d53130
John Dominic Lagarde <beemo453@gmail.com>
2017-11-04 20:01:32 +0800

    Edited some messages

--- src/hooks/restrictUser.js
+++ src/hooks/restrictUser.js
@@ -3,9 +3,9 @@ const restrictUser = () => (hook) => {
     if ((hook.id).toString() === (hook.params.user._id).toString() || hook.params.user.role === 'admin') {
       return hook;
     }
-    throw new Error('User is restricted to do following action');
+    throw new Error('User is restricted to do the following action');
   }
-  throw new Error('Validate hook should be used as a before hook');
+  throw new Error('Restrict User hook should be used as a before hook');
 };
 
 export default restrictUser;
