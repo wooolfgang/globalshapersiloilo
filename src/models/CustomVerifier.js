4f3918e1aaa93aa98989b5020c059d895d832d84
Clyne Paulo Fuentes <general_clyne@yahoo.com>
2017-11-04 17:19:23 +0800

    Add CustomVerifier for filtering unique users

---
+++ src/models/CustomVerifier.js
@@ -0,0 +1,19 @@
+import { Verifier } from 'feathers-authentication-oauth2';
+
+class CustomVerifier extends Verifier {
+  constructor(app, options = {}) {
+    super(app, options);
+  }
+  async verify(req, accessToken, refreshToken, profile, done) {
+    const data = { profile, accessToken, refreshToken };    
+    const userService = await this.app.service('api/users');
+    const email = profile._json.email ? profile._json.email : profile._json.emails[0].value;
+    const result = await userService.find({ query: { email } });
+    const normalizeResult = await this._normalizeResult(result);
+    const hasSameEmail = result.length !== 0 ? true : false;
+    const newData = hasSameEmail ? await this._updateEntity(normalizeResult, data) : await this._createEntity(data);
+    done(null, newData);
+  }
+}
+
+export default CustomVerifier;
