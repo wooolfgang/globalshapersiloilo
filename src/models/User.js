dd62e7bca580bca181913a4256bd764c35ec4753
Li Arolf Rey <liarolfrrey@gmail.com>
2017-11-06 12:46:01 +0800

    feat: Remove customVerifier / Change user schema

--- src/models/User.js
+++ src/models/User.js
@@ -4,12 +4,12 @@ import Model from './Model';
 class User extends Model {
   static get schema() {
     return {
+      _id: Joi.optional(),
       username: Joi.string().min(6).max(30).required(),
       password: Joi.string().min(6).max(30).required(),
       email: Joi.string().email().required(),
       fullName: Joi.string().required(),
       birthDay: Joi.string(),
-      // birthDay: Joi.string().required(), temporarily
       address: Joi.string(),
       phoneNumber: Joi.string(),
       occupation: Joi.string(),
