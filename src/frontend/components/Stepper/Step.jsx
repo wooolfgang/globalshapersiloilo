e70059299931c055c46322985d3f07e3b305be57
Li Arolf Rey <liarolfrrey@gmail.com>
2017-11-10 20:53:22 +0800

    refactor: Stepper Component transition change :(

--- src/frontend/components/Stepper/Step.jsx
+++ src/frontend/components/Stepper/Step.jsx
@@ -5,7 +5,7 @@ const StyledDiv = styled.div`
   display: inline-block;
   opacity: ${props => (props.showed ? 1 : 0)};
   width: ${props => (props.showed ? '100%' : 0)};
-  transition: all 300ms;
+  transition: all 300ms ease-in-out;
 `;
 
 const Step = ({ children, showed }) => (
