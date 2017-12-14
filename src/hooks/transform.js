920889ca74c59ebc1264ab0c2370e23d271c62a7
Li Arolf Rey <liarolfrrey@gmail.com>
2017-12-14 12:03:53 +0800

    refactor: Update transform hook for paginated queries

--- src/hooks/transform.js
+++ src/hooks/transform.js
@@ -1,12 +1,17 @@
-const transform = Model => (hookDetails) => {
-  const key = hookDetails.type === 'before' ? 'data' : 'result';
+const transform = Model => (hook) => {
+  const key = hook.type === 'before' ? 'data' : 'result';
+  const newHook = { ...hook };
 
-  if (hookDetails[key] instanceof Array) {
-    const transformData = hookDetails[key].map(data => new Model(data));
-    hookDetails[key] = transformData;
+  if (newHook[key].total) { // if service uses paginate
+    newHook[key].data = newHook[key].data.map(data => new Model(data));
+  } else if (newHook[key] instanceof Array) {
+    const transformData = newHook[key].map(data => new Model(data));
+    newHook[key] = transformData;
   } else {
-    hookDetails[key] = new Model(hookDetails[key]);
+    newHook[key] = new Model(newHook[key]);
   }
+
+  return newHook;
 };
 
 export default transform;
