8446ebeb7903e49323fc13c7dcda18b7b7aa2c66
Li Arolf Rey <liarolfrrey@gmail.com>
2017-12-26 09:35:20 +0800

    feat: Make Modal component close on outside click

--- src/frontend/components/Modal.jsx
+++ src/frontend/components/Modal.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { node, bool, func, string } from 'prop-types';
 
 const ModalOuter = styled.div`
   width: 100vw;
@@ -12,27 +13,69 @@ const ModalOuter = styled.div`
   background-color: rgb(0,0,0);
   background-color: rgba(0,0,0,0.4); 
   transform: ${props => (props.showed ? 'scale(1)' : 'scale(0)')};
+  z-index: 9999999;
 `;
 
 const ModalInner = styled.div`
   margin: auto;
   background: white;
-  border-radius: 5px;
+  border-radius: 3px;
   padding: 5px;
   transition: all 200ms;
-  width: ${props => (props.width ? props.width : '300px')};
-  height: ${props => (props.height ? props.height : '100px')};
+  width: ${props => (props.width && props.width)};
+  height: ${props => (props.height && props.height)};
   opacity: ${props => (props.showed ? 1 : 0)};
   transform: ${props => (props.showed ? 'scale(1)' : 'scale(0)')};
   box-shadow: ${props => props.theme.cardOne};
+  z-index: 9999999;
+  position: relative;
 `;
 
 class Modal extends React.Component {
+  static propTypes = {
+    children: node,
+    showed: bool.isRequired,
+    closeModal: func,
+    width: string,
+    height: string,
+  };
+
+  static defaultProps = {
+    closeModal: undefined,
+    children: undefined,
+    width: '300px',
+    height: '100px',
+  }
+
+  componentDidMount() {
+    document.addEventListener('click', this.handleOutsideClick, false);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.handleOutsideClick, false);
+  }
+
+  handleOutsideClick = (e) => {
+    if (!this.innerModal.contains(e.target) && this.outerModal.contains(e.target)) {
+      this.props.closeModal();
+    }
+  }
+
   render() {
-    const { children, showed } = this.props;
+    const { children, showed, width, height } = this.props;
     return (
-      <ModalOuter showed={showed}>
-        <ModalInner showed={showed}> {children} </ModalInner>
+      <ModalOuter
+        showed={showed}
+        innerRef={(modalNode) => { this.outerModal = modalNode; }}
+      >
+        <ModalInner
+          showed={showed}
+          innerRef={(modalNode) => { this.innerModal = modalNode; }}
+          width={width}
+          height={height}
+        >
+          {children}
+        </ModalInner>
       </ModalOuter>
     );
   }
