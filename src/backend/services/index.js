59b3c3c0f208ac0d6970a7675aa5095c20a99601
Li Arolf Rey <liarolfrrey@gmail.com>
2018-01-08 22:39:20 +0800

    feat: Add projectchat service

--- src/backend/services/index.js
+++ src/backend/services/index.js
@@ -5,6 +5,7 @@ import organizationService from './organization';
 import postService from './post';
 import userfollowerService from './userfollower';
 import projectvolunteer from './projectvolunteer';
+import projectchat from './projectchat';
 
 function services(db) {
   return function execute() {
@@ -17,6 +18,7 @@ function services(db) {
     app.configure(postService(db));
     app.configure(userfollowerService(db));
     app.configure(projectvolunteer(db));
+    app.configure(projectchat(db));
   };
 }
 
---
+++ src/backend/services/projectchat.js
@@ -0,0 +1,56 @@
+import feathersMongo from 'feathers-mongodb';
+import { fastJoin, deleteByDot } from 'feathers-hooks-common';
+import auth from '@feathersjs/authentication';
+import transformToObjectId from '../../hooks/transformToObjectId';
+import restrictToOwner from '../../hooks/restrictToOwner';
+
+function projectChatService(db) {
+  return function plugin() {
+    const app = this;
+    app.use('api/projectchat', feathersMongo({ Model: db.collection('projectchat') }));
+
+    const chatResolvers = {
+      joins: {
+        sentBy: $select => async (projectChat) => {
+          projectChat.sentBy = (await app.service('api/users').get(projectChat.sentById, { query: { $select } }));
+        },
+      },
+    };
+
+    const deleteExcessProps = () => (hook) => {
+      deleteByDot(hook.data, 'sentBy.projects');
+      deleteByDot(hook.data, 'sentBy.projectIds');
+    };
+
+    const hooks = {
+      authentication: [auth.hooks.authenticate('jwt')],
+      authorization: [restrictToOwner({ userIdField: '_id', ownerField: 'sentById' })],
+    };
+
+    app.service('api/projectchat').hooks({
+      before: {
+        find: [],
+        get: [],
+        create: [
+          transformToObjectId({ propertyFields: ['sentById'] }),
+          fastJoin(chatResolvers, { sentBy: [['fullName', 'imgUrl']] }),
+          deleteExcessProps(),
+        ],
+        update: [...hooks.authentication, ...hooks.authorization],
+        patch: [...hooks.authentication, ...hooks.authorization],
+        remove: [...hooks.authentication, ...hooks.authorization],
+      },
+      after: {
+        find: [],
+        get: [],
+        create: [],
+        update: [],
+        patch: [],
+        remove: [],
+        all: [],
+      },
+    });
+  };
+}
+
+export default projectChatService;
