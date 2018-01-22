306c3bf268b5bdb4530b06737376c26b47f05163
Li Arolf Rey <liarolfrrey@gmail.com>
2018-01-22 23:54:30 +0800

    fix: Remove crowd image on signin

--- src/frontend/components/Signin/Slogan.jsx
+++ src/frontend/components/Signin/Slogan.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { crowd } from '../../assets/theme/images';
 
 const StyledDiv = styled.div`
   position: relative;
@@ -42,28 +41,12 @@ const Motto = styled.p`
   } 
 `;
 
-const Image = styled.div`
-  position: absolute;
-  background: url(${crowd});
-  background-size: contain;
-  width: 80%;
-  left: 50%;
-  transform: translateX(-50%);
-  height: 210px;
-  top: 240px;
-
-  @media screen and (max-width: 1100px) {
-    display: none;
-  }
-`;
-
 const Slogan = () => (
   <StyledDiv>
     <Heading>
       <Title> Building a movement </Title>
       <Motto> We believe in a world where young people are a central to solution building, policy-making and lasting change. </Motto>
     </Heading>
-    <Image />
   </StyledDiv>
 );
 
